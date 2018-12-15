d87bb5aec86ddea93fb041300d6afffa80f61b02
RyanTseng <ryan10328@gmail.com>
2018-12-16 00:00:41 +0800

    add ReactiveFormsModule and finished the sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   data: any[] = [
     {title: 'aaa', subTitle: 'aaaa', content: 'aaaaa'},
     {title: 'bbb', subTitle: 'bbbb', content: 'bbbbb'},
     {title: 'ccc', subTitle: 'cccc', content: 'ccccc'}
   ];
 
+  frm: FormGroup;
   obj: any = {};
+
+  ngOnInit() {
+    this.frm = new FormGroup({
+      tabControl: new FormControl({value: '', disabled: false})
+    });
+  }
+
 }
